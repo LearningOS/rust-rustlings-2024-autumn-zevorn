f2a1a8d455126599eb130aa2ad619c6a0729e2f0
Chao Liu <chao.liu@yeah.net>
2024-09-25 21:46:35 +0800

    exercises/traits: Complete the exercise4

--- exercises/traits/traits4.rs
+++ exercises/traits/traits4.rs
@@ -5,9 +5,7 @@
 // Don't change any line other than the marked one.
 //
 // Execute `rustlings hint traits4` or use the `hint` watch subcommand for a
-// hint.
 
-// I AM NOT DONE
 
 pub trait Licensed {
     fn licensing_info(&self) -> String {
@@ -19,11 +17,19 @@ struct SomeSoftware {}
 
 struct OtherSoftware {}
 
-impl Licensed for SomeSoftware {}
-impl Licensed for OtherSoftware {}
+impl Licensed for SomeSoftware {
+    fn licensing_info(&self) -> String {
+        "some information".to_string()
+    }
+}
+impl Licensed for OtherSoftware {
+    fn licensing_info(&self) -> String {
+        "some information".to_string()
+    }
+}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn compare_license_types(software: ??, software_two: ??) -> bool {
+fn compare_license_types(software: SomeSoftware, software_two: OtherSoftware) -> bool {
     software.licensing_info() == software_two.licensing_info()
 }
 
@@ -44,6 +50,6 @@ mod tests {
         let some_software = SomeSoftware {};
         let other_software = OtherSoftware {};
 
-        assert!(compare_license_types(other_software, some_software));
+        assert!(compare_license_types(some_software, other_software));
     }
 }
