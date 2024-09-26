33fb15f4d03ef6ccf317397d6521f1c2c59778c4
Chao Liu <chao.liu@yeah.net>
2024-09-26 22:57:51 +0800

    exercises/tests: Complete the exercise5

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,18 +27,13 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
 
-extern "Rust" {
-    fn my_demo_function(a: u32) -> u32;
-    fn my_demo_function_alias(a: u32) -> u32;
+pub fn my_demo_function(a: u32) -> u32 {
+    a
 }
 
-mod Foo {
-    // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
-        a
-    }
+pub fn my_demo_function_alias(a: u32) -> u32 {
+    a
 }
 
 #[cfg(test)]
